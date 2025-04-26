672b1221d7dd6efa77c6dbf53f3a2a87f0ea915a
Sameer <164113933+Alishba56@users.noreply.github.com>
2025-04-26 22:22:54 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,40 @@
-"use client";
+"use client"
 
-import About from "@/components/about";
-import Hero from "@/components/home";
-import Education from "@/components/education";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import Interests from "@/components/interests";
-import Projects from "@/components/projects";
-import Skills from "@/components/skills";
+import About from "@/components/about"
+import Hero from "@/components/home"
+import Education from "@/components/education"
+import Contact from "@/components/contact"
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+import Interests from "@/components/interests"
+import Projects from "@/components/projects"
+import Skills from "@/components/skills"
+
+
+import { useState, useEffect } from "react"
+
+ 
 
 export default function Home() {
+  const [isContactOpen, setIsContactOpen] = useState(false)
+
+  useEffect(() => {
+    const storedContactState = localStorage.getItem("contactOpen")
+    if (storedContactState) {
+      setIsContactOpen(JSON.parse(storedContactState))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("contactOpen", JSON.stringify(isContactOpen))
+  }, [isContactOpen])
+
+
   return (
     <div className="flex  overflow-hidden">
       <Header />
-      <div>
-        <Hero />
+      <div >
+      <Hero/> 
         <Skills />
         <Education />
         <Interests />
@@ -22,6 +42,8 @@ export default function Home() {
         <Projects />
         <Footer />
       </div>
-    </div>
-  );
+      <Contact  isOpen={isContactOpen} onClose={() => setIsContactOpen(false)} />
+         </div>
+  )
 }
+
